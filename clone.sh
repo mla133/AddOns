351f80c9d8b696cd664d1a6a535a21ec39fa3305
Matt <matthew.l.allen@gmail.com>
2015-05-22 19:23:14 -0400

    Fixed $VERSION to 60100

--- clone.sh
+++ clone.sh
@@ -3,7 +3,7 @@
 # Also will accept a version number to update the Interface number in TOC file.
 
 CLONEDIR=$1
-VERSION=$2
+VERSION=60100
 cp -r wow-NewAddon/ $CLONEDIR
 cd $CLONEDIR
 mv NewAddon.lua $CLONEDIR.lua
