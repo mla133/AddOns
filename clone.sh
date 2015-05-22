84c4f0b84c8d40cc820403423b9d9a482d4d50e9
Matt Allen <matthew.allen@fmcti.com>
2015-05-22 11:42:57 -0400

    Updated Author desc in TOC file for template, also including a new script that clones the wow-NewAddon directory and renames files and file references in the TOC and LUA files.

---
+++ clone.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Bash script to clone NewAddon directory and rename the .lua & .toc files accordingly
+# Also will accept a version number to update the Interface number in TOC file.
+
+CLONEDIR=$1
+VERSION=$2
+cp -r wow-NewAddon/ $CLONEDIR
+cd $CLONEDIR
+mv NewAddon.lua $CLONEDIR.lua
+mv NewAddon.toc $CLONEDIR.toc
+sed -i -- "s/NewAddon/$CLONEDIR/g" $CLONEDIR.*
+sed -i -- "s/40300/$VERSION/g" $CLONEDIR.toc
